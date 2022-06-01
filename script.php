994b6d31254efd22855e094a75bb1c03f3266a05
omololevy <cotechlevy@gmail.com>
2022-06-01 17:55:03 +0300

    Add the function to get the headers

--- script.php
+++ script.php
@@ -4,4 +4,15 @@ $limit = 5555;
 $used = 0;
 $message = "";
 
+function getHeaders($curl, $header_line)
+{
+    if (strpos($header_line, "X-RateLimit-Limit:") !== false) {
+        $GLOBALS["limit"] = (int) preg_replace("/[^0-9]/", "", $header_line);
+    }
+    if (strpos($header_line, "X-RateLimit-Used:") !== false) {
+        $GLOBALS["used"] = (int) preg_replace("/[^0-9]/", "", $header_line);
+    }
+    return strlen($header_line);
+}
+
 ?>
